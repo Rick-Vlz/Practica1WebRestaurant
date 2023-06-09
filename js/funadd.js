b71769d4e4684be689d86efe7c55eece6a202f33
Rick-Vlz <ricardovlz.wlf@gmail.com>
2023-06-09 07:01:32 -0600

    xd

--- js/funadd.js
+++ js/funadd.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
+    $(document).ready(function(){
+        $('select').formSelect();
+    });
     $("p").eq(0).css("font-Style","italic");
     $("p").eq(1).css("fontWeight","bold");
-    $("#Wpic").css("display","none");
 
     $("p").eq(0).dblclick(function(){
-        $("#Wpic").css("display","block");
-        });
+        $("#Wpic").removeClass("hide");
+    });
+    $("p").eq(0).click(function(){
+        $("#Wpic").addClass("hide");
+    });
 });
